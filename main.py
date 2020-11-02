47a0054cc7f1a2be6d62c1deb152bee202804db3
Gellifree <forge.delfadortitan@gmail.com>
2020-11-02 14:41:59 +0100

    Sablon menürendszer kirajzolása A "Főprogram" elkészítve, készenáll befogadni a menüpontokat first(), second() névvel, de ezek módosíthtóak igény szerint. Nincs még színezve, vagy igazíŧva a szöveg

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print("Helló Világ!")
+#importing needed resources
+import os
+
+# Functions for drawing the menues:
+def drawMenu(elements):
+    index = 0
+    for element in elements:
+        if(element == "Kilépés"):
+            print("     [Q] {}".format(element))
+        else:
+            print("     [{}] {}".format(index, element))
+        index += 1
+    print()
+    answer = input(" >> ")
+    if(answer == "Q" or answer == "q"):
+        return answer
+    elif(int(answer) < len(elements)):
+        return answer
+    else:
+        print("  >> Nem adhatsz meg ilyen opciót! <<")
+        return "error"
+
+def main():
+    mainMenu = ["Első menüpont", "Második menüpont", "Kilépés"]
+    executableMenu = ["first()", "second()"]
+
+    menuState = 0
+    answer = 0
+    while(answer != "q" and answer != "Q"):
+        os.system("clear")
+        print("Számrendszer átváltó és feladatgeneráló")
+
+        answer = drawMenu(mainMenu)
+        if(answer == "error"):
+            input("  >> Kész <<")
+        elif(answer != "Q" and answer != "q"):
+            menuState = int(answer)
+            print(" >> " + mainMenu[menuState] + " <<\n")
+            exec(executableMenu[menuState])
+            input(" >> Kész <<")
+        else:
+            quit = input("  >> Biztos vagy benne, hogy ki szeretnél lépni? [Y/n]: ")
+            if(quit == "N" or quit == "n"):
+                answer = 0
+            elif(quit == "" or quit == "Y" or quit == "y"):
+                answer = "q"
+                os.system("clear")
+
+
+if(__name__ == "__main__"):
+    main()
