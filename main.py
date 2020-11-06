c77efac7d618431eb719bd5f54f46211b8254cad
Gellifree <forge.delfadortitan@gmail.com>
2020-11-06 14:35:05 +0100

    Convertáló függvény megírása, int és string tipusra

--- main.py
+++ main.py
@@ -63,6 +63,54 @@ def checkIfFractionExist(number):
             return True
     return False
 
+def safetyConvert(data):
+    if(type(data) == str):
+        result = {
+            "0" : 0,
+            "1" : 1,
+            "2" : 2,
+            "3" : 3,
+            "4" : 4,
+            "5" : 5,
+            "6" : 6,
+            "7" : 7,
+            "8" : 8,
+            "9" : 9,
+            "A" : 10,
+            "a" : 10,
+            "B" : 11,
+            "b" : 11,
+            "C" : 12,
+            "c" : 12,
+            "D" : 13,
+            "d" : 13,
+            "E" : 14,
+            "e" : 14,
+            "F" : 15,
+            "f" : 15
+        }
+        return result.get(data, "rorrE")
+    elif(type(data) == int):
+        result = {
+            0 : "0",
+            1 : "1",
+            2 : "2",
+            3 : "3",
+            4 : "4",
+            5 : "5",
+            6 : "6",
+            7 : "7",
+            8 : "8",
+            9 : "9",
+            10: "A",
+            11: "B",
+            12: "C",
+            13: "D",
+            14: "E",
+            15: "F"
+        }
+        return result.get(data, "rorrE")
+
 
 def first():
     pass
