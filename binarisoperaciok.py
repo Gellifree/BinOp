1270cbde9c1afa2d8dae0c6dc240e5381d288287
evilyear <evilyear@gmail.com>
2020-11-23 15:05:27 +0100

    bináris összeadás
    #tesztelve kiiratással, számok megadásával
    #következő lépésben össze kell dolgozni a már meglévő funkciókat és kidolgozni a kivonás és szorzás műveletét majd push ellenőrzésre

--- binarisoperaciok.py
+++ binarisoperaciok.py
@@ -295,6 +295,26 @@ def oneComplementer():
 
     print("  Az egyes komplementer: ", result)
 
+def additionBinary(x, y):
+    max_len = max(len(x), len(y))
+
+    x = x.zfill(max_len)
+    y = y.zfill(max_len)
+
+    result = ''
+    carry = 0
+
+    for i in range(max_len - 1, -1, -1):
+        r = carry
+        r += 1 if x[i] == '1' else 0
+        r += 1 if y[i] == '1' else 0
+        result = ('1' if r % 2 == 1 else '0') + result
+        carry = 0 if r < 2 else 1  # Compute the carry.
+
+    if carry != 0: result = '1' + result
+
+    return result.zfill(max_len)
+
 
 def main():
     mainMenu = ["Átváltás tízes számrendszerből", "Átváltás tízes számrendszerbe", "Feladatsor generálása","Feladatsorok megtekintése","Beállítások","Segítség","Egyes komplementer", "Kilépés"]
